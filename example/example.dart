3558b11a4e47472e0e3e47f7073864eb47c410fa
redDwarf03 <rddwarf03@gmail.com>
2021-07-20 13:29:09 +0200

    update example with new import

--- example/example.dart
+++ example/example.dart
@@ -1,11 +1,9 @@
-import 'package:archethic_lib_dart/src/crypto.dart' as crypto
-    show deriveKeyPair;
-import 'package:archethic_lib_dart/src/model/key_pair.dart';
-import 'package:archethic_lib_dart/src/transaction_builder.dart';
+import 'package:archethic_lib_dart/archethic_lib_dart.dart'
+    show KeyPair, deriveKeyPair, TransactionBuilder;
 
 void main(List<String> args) {
   /// It creates a new keypair into hexadecimal format
-  KeyPair keypair = crypto.deriveKeyPair("mysuperpassphraseorseed", 0);
+  KeyPair keypair = deriveKeyPair("mysuperpassphraseorseed", 0);
 
   /// Generate `address`, `timestamp`, `previousPublicKey`, `previousSignature`, `originSignature` of the transaction and
   /// serialize it using a custom binary protocol.
@@ -16,7 +14,7 @@ void main(List<String> args) {
       .build("mysuperpassphraseorseed", 0, 'P256');
 
   /// Sign the transaction with an origin device private key
-  final KeyPair originKeypair = crypto.deriveKeyPair('origin_seed', 0);
+  final KeyPair originKeypair = deriveKeyPair('origin_seed', 0);
   var transaction2 = new TransactionBuilder("transfer")
       .addUCOTransfer(
           "00b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646",
