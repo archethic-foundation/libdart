8e6b3394d99ef141fac9a90a31837f6d12bc5954
redDwarf03 <rddwarf03@gmail.com>
2021-10-27 19:15:12 +0200

    Null check operator used on a null value #6

--- example/example.dart
+++ example/example.dart
@@ -8,7 +8,7 @@ void main(List<String> args) {
 
   /// Generate `address`, `timestamp`, `previousPublicKey`, `previousSignature`, `originSignature` of the transaction and
   /// serialize it using a custom binary protocol.
-  final Transaction transaction = Transaction(type: 'transfer')
+  final Transaction transaction = Transaction(type: 'transfer', data: Transaction.initData())
       .addUCOTransfer(
           '00b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646',
           0.420)
@@ -17,7 +17,7 @@ void main(List<String> args) {
 
   /// Sign the transaction with an origin device private key
   final KeyPair originKeypair = deriveKeyPair('origin_seed', 0);
-  final Transaction transaction2 = Transaction(type: 'transfer')
+  final Transaction transaction2 = Transaction(type: 'transfer', data: Transaction.initData())
       .addUCOTransfer(
           '00b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646',
           0.420)
@@ -26,7 +26,7 @@ void main(List<String> args) {
   transaction2.convertToJSON();
 
   /// Export the transaction generated into JSON
-  final Transaction transaction3 = Transaction(type: 'transfer')
+  final Transaction transaction3 = Transaction(type: 'transfer', data: Transaction.initData())
       .addUCOTransfer(
           '00b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646',
           0.420)
