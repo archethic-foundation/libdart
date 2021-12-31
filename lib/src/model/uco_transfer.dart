5fed22edafbc4c51ae3b837eeac8be75f65b2aa4
redDwarf03 <rddwarf03@gmail.com>
2021-12-31 18:48:52 +0100

    fix bigint value

--- lib/src/model/uco_transfer.dart
+++ lib/src/model/uco_transfer.dart
@@ -12,8 +12,7 @@ class UCOTransfer {
   String? to;
 
   factory UCOTransfer.fromJson(Map<String, dynamic> json) => UCOTransfer(
-        amount:
-            json['amount'] == null ? null : toBigInt(json['amount'].toDouble()),
+        amount: json['amount'] == null ? null : toBigInt(json['amount']),
         to: json['to'],
       );
 
