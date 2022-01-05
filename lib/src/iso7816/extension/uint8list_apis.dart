4327381346defcc7cee08acbd95b433e91a96d9c
redDwarf03 <rddwarf03@gmail.com>
2022-01-05 13:55:15 +0100

    APDU test

---
+++ lib/src/iso7816/extension/uint8list_apis.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:convert/convert.dart' as C;
+
+extension Uint8ListEncodeApis on Uint8List {
+  String base64() {
+    return Base64Codec().encode(this);
+  }
+
+  String hex() {
+    return C.hex.encoder.convert(this);
+  }
+}
---
+++ test/apdu_test.dart
@@ -0,0 +1,11 @@
+import 'package:archethic_lib_dart/src/iso7816/command_apdu.dart';
+import 'package:test/test.dart';
+import 'package:archethic_lib_dart/src/iso7816/extension/string_apis.dart';
+
+void main() {
+  test('Command APDU test', () {
+    // Get ArchEthic Account Address
+    expect(CommandAPDU(cla: 0xe0, ins: 0x04, p1: 0, p2: 0).toBytes(),
+        "e00400".parseHex());
+  });
+}
