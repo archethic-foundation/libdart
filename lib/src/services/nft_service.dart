dabe696f33fa51120791b21d007fef6e1c0c4e95
redDwarf03 <rddwarf03@gmail.com>
2022-01-09 17:32:44 +0100

    NFT - add document in content

--- lib/src/services/nft_service.dart
+++ lib/src/services/nft_service.dart
@@ -10,8 +10,12 @@ class NFTService {
   /// @param {String} [curve] Elliptic curve to use ("ed25519", "P256", "secp256k1")
   /// @param {} [originPrivateKey] The origin signature to able to perform the ProofOfWork and authorize the transaction
   Transaction prepareNewNFT(int initialSupply, String name, String seed,
-      int index, String curve, String originPrivateKey) {
-    final String content = 'initial supply: $initialSupply\nname: $name';
+      int index, String curve, String originPrivateKey,
+      {String? typeMime, String? documentHex}) {
+    String content = 'initial supply: $initialSupply\nname: $name';
+    if (typeMime != null && documentHex != null) {
+      content = content + '\ntypeMime: $typeMime\ndocument: $documentHex';
+    }
     return Transaction(type: 'nft', data: Transaction.initData())
         .setContent(content)
         .build(seed, index, curve)
