33e9491444ff643f4268e5eaeda40f65b00450a1
redDwarf03 <rddwarf03@gmail.com>
2022-02-03 07:32:56 +0100

    remove apdu management

--- lib/src/iso7816/extension/datetime_apis.dart
+++
@@ -1,8 +0,0 @@
-extension DateTimeYYMMDDFormatApi on DateTime {
-  String formatYYMMDD() {
-    final String y = year.toString().substring(2, 4).padLeft(2, '0');
-    final String m = month.toString().padLeft(2, '0');
-    final String d = day.toString().padLeft(2, '0');
-    return y + m + d;
-  }
-}
--- lib/src/iso7816/extension/int_apis.dart
+++
@@ -1,7 +0,0 @@
-extension IntApis on int {
-  String hex() {
-    final String str = toRadixString(16);
-    final int paddedLen = (str.length.isOdd ? 1 : 0) + str.length;
-    return str.padLeft(paddedLen, '0').toUpperCase();
-  }
-}
