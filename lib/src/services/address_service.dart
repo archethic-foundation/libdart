436bc3e2b7a71897aac41351d5d635e9e07d4137
redDwarf03 <rddwarf03@gmail.com>
2022-02-24 17:45:26 +0100

    Prepend curve type in address

--- lib/src/services/address_service.dart
+++ lib/src/services/address_service.dart
@@ -1,6 +1,8 @@
 /// SPDX-License-Identifier: AGPL-3.0-or-later
 
 // Project imports:
+import 'dart:typed_data';
+
 import 'package:archethic_lib_dart/src/utils/crypto.dart' as crypto
     show deriveKeyPair, hash;
 
@@ -8,7 +10,8 @@ import 'package:archethic_lib_dart/src/utils/crypto.dart' as crypto
 import 'package:archethic_lib_dart/src/model/crypto/key_pair.dart';
 import 'package:archethic_lib_dart/src/model/transaction.dart';
 import 'package:archethic_lib_dart/src/services/api_service.dart';
-import 'package:archethic_lib_dart/src/utils/utils.dart' show uint8ListToHex;
+import 'package:archethic_lib_dart/src/utils/utils.dart'
+    show uint8ListToHex, concatUint8List;
 
 class AddressService {
   AddressService(this.endpoint);
@@ -16,19 +19,6 @@ class AddressService {
   /// [endpoint] is the HTTP URL to a ArchEthic node (acting as welcome node)
   String? endpoint;
 
-  /// Derive an address
-  /// @param {String} seed TransactionChain seed
-  /// @param {int} index Number of transaction in the chain
-  /// @param {String} curve  Elliptic curve to use ("ed25519", "P256", "secp256k1")
-  /// @param {String} hashAlgo  Hash algorithm ("sha256", "sha512", "sha3-256", "sha3-512", "blake2b")
-  String deriveAddress(String seed, int index,
-      {String curve = 'P256', String hashAlgo = 'sha256'}) {
-    final KeyPair keypair = crypto.deriveKeyPair(seed, index, curve: curve);
-    return uint8ListToHex(crypto.hash(keypair.publicKey, algo: hashAlgo));
-  }
-
-  /*
-  TODO: Wait for https://github.com/archethic-foundation/archethic-node/pull/210
   /// Derive an address
   /// @param {String} seed TransactionChain seed
   /// @param {int} index Number of transaction in the chain
@@ -57,7 +47,6 @@ class AddressService {
         throw 'Curve not supported';
     }
   }
-  */
 
   /// Get the last address from seed
   /// @param {String} seed TransactionChain seed
