9563c46ad9acfbfa851df4c8bb37ede84a4bb957
redDwarf03 <rddwarf03@gmail.com>
2022-03-17 19:17:07 +0100

    #14 (json part)

---
+++ lib/src/model/response/origin_public_key_response.dart
@@ -0,0 +1,32 @@
+// To parse this JSON data, do
+//
+//     final originPublicKeyResponse = originPublicKeyResponseFromJson(jsonString);
+
+import 'dart:convert';
+
+OriginPublicKeyResponse originPublicKeyResponseFromJson(String str) =>
+    OriginPublicKeyResponse.fromJson(json.decode(str));
+
+String originPublicKeyResponseToJson(OriginPublicKeyResponse data) =>
+    json.encode(data.toJson());
+
+class OriginPublicKeyResponse {
+  OriginPublicKeyResponse({
+    this.certificate,
+    this.originPublicKey,
+  });
+
+  String? certificate;
+  String? originPublicKey;
+
+  factory OriginPublicKeyResponse.fromJson(Map<String, dynamic> json) =>
+      OriginPublicKeyResponse(
+        certificate: json["certificate"],
+        originPublicKey: json["originPublicKey"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "certificate": certificate,
+        "originPublicKey": originPublicKey,
+      };
+}
