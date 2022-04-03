2fb547b38c6f4a6e9ef705ede9e2e457f341d204
redDwarf03 <rddwarf03@gmail.com>
2022-04-03 13:46:19 +0200

    Add hashAlgo param in prepareNewNFT method

--- lib/src/services/nft_service.dart
+++ lib/src/services/nft_service.dart
@@ -10,17 +10,21 @@ class NFTService {
   /// @param {String} [seed] TransactionChain seed
   /// @param {int} [index] Number of transaction in the chain
   /// @param {String} [curve] Elliptic curve to use ("ed25519", "P256", "secp256k1")
+  /// @param {String} [hashAlgo] Hash algorithm ("sha256", "sha512", "sha3-256", "sha3-512", "blake2b")
   /// @param {} [originPrivateKey] The origin signature to able to perform the ProofOfWork and authorize the transaction
   Transaction prepareNewNFT(int initialSupply, String name, String seed,
-      int index, String curve, String originPrivateKey,
-      {String? typeMime, String? documentHex}) {
+      int index, String originPrivateKey,
+      {String? typeMime,
+      String? documentHex,
+      String? curve,
+      String? hashAlgo}) {
     String content = 'initial supply: $initialSupply\nname: $name';
     if (typeMime != null && documentHex != null) {
       content = content + '\ntypeMime: $typeMime\ndocument: $documentHex';
     }
     return Transaction(type: 'nft', data: Transaction.initData())
         .setContent(content)
-        .build(seed, index, curve: curve)
+        .build(seed, index, curve: curve!, hashAlgo: hashAlgo!)
         .originSign(originPrivateKey);
   }
 }
