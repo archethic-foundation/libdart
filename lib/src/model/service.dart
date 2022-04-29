d96b95c6eeba42af3e2261bfefea62fe1cae28ab
redDwarf03 <rddwarf03@gmail.com>
2022-04-29 21:43:00 +0200

    add licensing

--- lib/src/model/service.dart
+++ lib/src/model/service.dart
@@ -1,3 +1,7 @@
+/// SPDX-License-Identifier: AGPL-3.0-or-later
+/// Package archEthic aims to provide a easy way to create Archethic transaction and to send them over the network.
+///
+/// This implementation is based on Official Archethic Javascript library for Node and Browser.
 class Service {
   Service({this.derivationPath, this.curve, this.hashAlgo});
 
