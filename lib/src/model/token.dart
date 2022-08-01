1797898ba546d6b8425c6d3fd4830da854c00d77
redDwarf03 <rddwarf03@gmail.com>
2022-08-01 23:23:50 +0200

    json encode bigint...

--- lib/src/model/token.dart
+++ lib/src/model/token.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
 
-import 'package:archethic_lib_dart/src/utils/utils.dart';
-
 /// SPDX-License-Identifier: AGPL-3.0-or-later
 
 // To parse this JSON data, do
@@ -10,10 +8,15 @@ import 'package:archethic_lib_dart/src/utils/utils.dart';
 
 Token tokenFromJson(String str) => Token.fromJson(json.decode(str));
 
-String tokenToJson(Token data) => json.encode(data.toJson());
+String tokenToJson(Token data) => jsonEncode(data.toJson())
+    .replaceAll('supply":"', 'supply":')
+    .replaceAll('","type"', ',"type"');
 
 String tokenToJsonForTxDataContent(Token data) {
-  return '{"name": "${data.name!}", "supply": ${data.supply}, "type": "${data.type!}", "symbol": "${data.symbol!}", "properties":[]';
+  // Sorry for that....
+  return jsonEncode(data.toJsonForTxDataContent())
+      .replaceAll('supply":"', 'supply":')
+      .replaceAll('","type"', ',"type"');
 }
 
 class Token {
@@ -32,41 +35,53 @@ class Token {
   String? symbol;
   List<TokenProperty>? tokenProperties = <TokenProperty>[];
 
-  factory Token.fromJson(Map<String, dynamic> json) => Token(
-        address: json['address'],
-        name: json['name'],
-        supply:
-            json['supply'] == null ? null : toBigInt(json['supply'].toDouble()),
-        type: json['type'],
-        symbol: json['symbol'],
-        tokenProperties: json['properties'] == null
-            ? null
-            : List<TokenProperty>.from(json['properties'].map((x) => x)),
-      );
+  factory Token.fromJson(Map<String, dynamic> json) {
+    return Token(
+      address: json['address'],
+      name: json['name'],
+      supply: _BigIntSerializer.deserialize(json['supply']),
+      type: json['type'],
+      symbol: json['symbol'],
+      tokenProperties: json['properties'] == null
+          ? null
+          : List<TokenProperty>.from(json['properties'].map((x) => x)),
+    );
+  }
 
-  Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => <String, dynamic>{
         'address': address,
         'name': name,
-        'supply': supply,
+        'supply': _BigIntSerializer.serialize(supply!),
         'type': type,
         'symbol': symbol,
         'properties': tokenProperties == null
             ? []
-            : List<dynamic>.from(tokenProperties!.map((x) => x)),
+            : List<dynamic>.from(tokenProperties!.map((TokenProperty x) => x)),
       };
 
-  Map<String, dynamic> toJsonForTxDataContent() => {
+  Map<String, dynamic> toJsonForTxDataContent() => <String, dynamic>{
         'name': name,
-        'supply': supply,
+        'supply': _BigIntSerializer.serialize(supply!),
         'type': type,
         'symbol': symbol,
         'properties': tokenProperties == null
             ? []
-            : List<dynamic>.from(tokenProperties!.map((x) => x)),
+            : List<dynamic>.from(tokenProperties!.map((TokenProperty x) => x)),
       };
 }
 
 class TokenProperty {
   String? name;
   String? value;
+}
+
+class _BigIntSerializer {
+  static BigInt deserialize(Object? value) {
+    final String json = value as String;
+    return BigInt.parse(json);
+  }
+
+  static String? serialize(BigInt value) {
+    return value.toString();
+  }
 }
