41c8763da59fef50025ae92342e71eed8831faca
redDwarf03 <rddwarf03@gmail.com>
2022-08-02 16:46:19 +0200

    add serialization method

--- lib/src/model/token_fungible.dart
+++ lib/src/model/token_fungible.dart
@@ -1,7 +1,18 @@
 /// SPDX-License-Identifier: AGPL-3.0-or-later
 
 // Flutter imports:
+import 'dart:convert';
+
 import 'package:archethic_lib_dart/src/model/token.dart';
+
+TokenFungible tokenFungibleFromJson(String str) =>
+    TokenFungible.fromJson(json.decode(str));
+
+String tokenFungibleToJson(TokenFungible data) => jsonEncode(data.toJson());
+
+String tokenFungibleToJsonForTxDataContent(TokenFungible data) {
+  return jsonEncode(data.toJsonForTxDataContent());
+}
 
 class TokenFungible extends Token {
   static const String kType = 'fungible';
@@ -13,4 +24,19 @@ class TokenFungible extends Token {
       super.type = kType,
       required super.symbol,
       super.tokenProperties});
+
+  factory TokenFungible.fromJson(Map<String, dynamic> json) {
+    return TokenFungible(
+      address: json['address'],
+      name: json['name'],
+      supply: json['supply'] == null
+          ? null
+          : int.tryParse(json['supply'].toString()),
+      type: kType,
+      symbol: json['symbol'],
+      tokenProperties: json['properties'] == null
+          ? null
+          : List<TokenProperty>.from(json['properties'].map((x) => x)),
+    );
+  }
 }
