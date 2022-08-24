8ccaa29d25f2e2e0e7226c5e221c44187699082d
redDwarf03 <rddwarf03@gmail.com>
2022-08-24 15:44:14 +0200

    fix import

--- lib/src/model/ledger_operations.dart
+++ lib/src/model/ledger_operations.dart
@@ -1,7 +1,6 @@
 /// SPDX-License-Identifier: AGPL-3.0-or-later
 
 // Project imports:
-import 'package:archethic_lib_dart/src/model/node_movement.dart';
 import 'package:archethic_lib_dart/src/model/transaction_movement.dart';
 import 'package:archethic_lib_dart/src/model/unspent_outputs.dart';
 
