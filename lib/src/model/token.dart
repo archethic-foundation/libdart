68d35d01d714d632ed6d0c6ea6927c2f27c56e54
redDwarf03 <rddwarf03@gmail.com>
2023-02-08 15:21:42 +0100

    change tokenToJsonForTxDataContent signature

--- lib/src/model/token.dart
+++ lib/src/model/token.dart
@@ -24,8 +24,8 @@ class Token with _$Token {
 
   factory Token.fromJson(Map<String, dynamic> json) => _$TokenFromJson(json);
 
-  String tokenToJsonForTxDataContent(Token data) {
-    return jsonEncode(data.toJsonForTxDataContent());
+  String tokenToJsonForTxDataContent() {
+    return jsonEncode(toJsonForTxDataContent());
   }
 
   Map<String, dynamic> toJsonForTxDataContent() => <String, dynamic>{
