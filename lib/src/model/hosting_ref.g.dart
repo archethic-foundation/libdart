6bc2c19841d6b4285dfaca069b50cd47226ef1e8
redDwarf03 <rddwarf03@gmail.com>
2023-03-25 22:16:18 +0100

    Rename Hosting class

--- lib/src/model/hosting_ref.g.dart
+++ lib/src/model/hosting_ref.g.dart
@@ -1,22 +1,23 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'hosting.dart';
+part of 'hosting_ref.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-_$_Hosting _$$_HostingFromJson(Map<String, dynamic> json) => _$_Hosting(
+_$_HostingRef _$$_HostingRefFromJson(Map<String, dynamic> json) =>
+    _$_HostingRef(
       aewebVersion: json['aewebVersion'] as int? ?? 1,
       hashFunction: json['hashFunction'] as String? ?? 'sha1',
       metaData: (json['metaData'] as Map<String, dynamic>?)?.map(
-            (k, e) => MapEntry(
-                k, HostingContentMetaData.fromJson(e as Map<String, dynamic>)),
+            (k, e) => MapEntry(k,
+                HostingRefContentMetaData.fromJson(e as Map<String, dynamic>)),
           ) ??
           const {},
     );
 
-Map<String, dynamic> _$$_HostingToJson(_$_Hosting instance) =>
+Map<String, dynamic> _$$_HostingRefToJson(_$_HostingRef instance) =>
     <String, dynamic>{
       'aewebVersion': instance.aewebVersion,
       'hashFunction': instance.hashFunction,
