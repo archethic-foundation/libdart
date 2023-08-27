a9fd1f89cd954a49f25d28bcf01737cca90ab9ea
redDwarf03 <rddwarf03@gmail.com>
2023-08-27 09:33:11 +0200

    Add a feature flag for Wrap transaction inside an object named transaction

--- lib/src/utils/json_rpc_util.dart
+++ lib/src/utils/json_rpc_util.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:archethic_lib_dart/features_flags.dart';
 import 'package:archethic_lib_dart/src/model/exception/archethic_json_rpc_exception.dart';
 
 /// SPDX-License-Identifier: AGPL-3.0-or-later
@@ -20,9 +21,21 @@ mixin JsonRPCUtil {
   }
 
   Map<String, dynamic> getJsonRPCResult(String body) {
-    return json.decode(
-      getJsonRPCResultString(body),
-    );
+    if (FeatureFlags.namedActions) {
+      return json.decode(
+        getJsonRPCResultString(body),
+      );
+    } else {
+      final jsonResponse = json.decode(body);
+      if (jsonResponse.containsKey('error')) {
+        throw ArchethicJsonRPCException(
+          code: jsonResponse['error']['code'],
+          message: jsonResponse['error']['message'],
+          data: jsonResponse['error']['data'],
+        );
+      }
+      return jsonResponse['result'];
+    }
   }
 
   String getJsonRPCResultString(String body) {
