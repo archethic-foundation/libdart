b2e7db961cffa4d8fea731a0da77c583c909b7f8
Chralu <contact@charly-caulet.net>
2024-12-12 15:54:58 +0100

    docs: :bulb: Update root comment.

--- lib/archethic_lib_dart.dart
+++ lib/archethic_lib_dart.dart
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
+/// SPDX-License-Identifier: AGPL-3.0-or-later
+///
 /// Package archEthic aims to provide a easy way to create Archethic transaction and to send them over the network.
 ///
 /// This implementation is based on Official Archethic Javascript library for Node and Browser.
