280c7221de9267477190751e4605bd7eac978dfd
Chralu <contact@charly-caulet.net>
2024-12-16 10:53:13 +0100

    fix: :bug: Oracle unsubscribe does not work #155

--- lib/src/services/oracle_service.dart
+++ lib/src/services/oracle_service.dart
@@ -2,17 +2,18 @@
 import 'dart:async';
 
 import 'package:archethic_lib_dart/src/model/oracle_chain/oracle_uco_price.dart';
-import 'package:archethic_lib_dart/src/model/response/oracle_data_response.dart';
-import 'package:archethic_lib_dart/src/model/uco.dart';
 import 'package:archethic_lib_dart/src/utils/oracle/archethic_oracle.dart';
-import 'package:http/http.dart' as http show post;
 import 'package:logging/logging.dart';
 
 class OracleService {
   OracleService(this.endpoint);
 
   /// [endpoint] is the HTTP URL to a Archethic node (acting as welcome node)
-  String? endpoint;
+  final String endpoint;
+
+  late final _archethicOracle = ArchethicOracle(
+    endpoint: endpoint,
+  );
 
   final _logger = Logger('OracleService');
 
@@ -20,89 +21,20 @@ class OracleService {
   ///
   /// if [timestamp] = 0 or not precised, the last price is returned.
   Future<OracleUcoPrice> getOracleData({int timestamp = 0}) async {
-    final completer = Completer<OracleUcoPrice>();
-    var oracleUcoPrice = const OracleUcoPrice(uco: Uco(eur: 0, usd: 0));
-    final requestHeaders = <String, String>{
-      'Content-type': 'application/json',
-      'Accept': 'application/json',
-    };
-    var body = '';
-
     try {
-      if (timestamp == 0) {
-        body =
-            '{"query": "query { oracleData { timestamp services { uco { eur, usd } } } }"}';
-      } else {
-        body =
-            '{"query": "query { oracleData(timestamp: $timestamp) { timestamp services { uco { eur, usd } } } }"}';
-      }
-
-      _logger.fine('getOracleData: requestHttp.body=$body');
-      final responseHttp = await http.post(
-        Uri.parse('${endpoint!}/api'),
-        body: body,
-        headers: requestHeaders,
-      );
-      _logger.fine('getOracleData: responseHttp.body=${responseHttp.body}');
-      if (responseHttp.statusCode == 200) {
-        final oracleDataResponse =
-            oracleDataResponseFromJson(responseHttp.body);
-        if (oracleDataResponse.data != null &&
-            oracleDataResponse.data!.oracleData != null &&
-            oracleDataResponse.data!.oracleData!.services != null &&
-            oracleDataResponse.data!.oracleData!.services!.uco != null) {
-          oracleUcoPrice = OracleUcoPrice(
-            timestamp: oracleDataResponse.data!.oracleData!.timestamp,
-            uco: Uco(
-              eur: oracleDataResponse.data!.oracleData!.services!.uco!.eur,
-              usd: oracleDataResponse.data!.oracleData!.services!.uco!.usd,
-            ),
-          );
-        }
-      }
+      final result = await _archethicOracle.getOracleData(timestamp: timestamp);
+      _logger.fine('getOracleData: $result');
+      return result;
     } catch (e, stack) {
       _logger.severe(
         'getOracleData failed',
         e,
         stack,
       );
+      return const OracleUcoPrice();
     }
-
-    completer.complete(oracleUcoPrice);
-    return completer.future;
   }
 
   /// Subscribe to notifications when a new oracle data is stored.
-  Future<ArchethicOracle> subscribeToOracleUpdates(
-    Function(OracleUcoPrice?) onUpdate,
-  ) async {
-    String websocketEndpoint;
-    switch (endpoint) {
-      case 'https://mainnet.archethic.net':
-      case 'https://testnet.archethic.net':
-        websocketEndpoint =
-            "${endpoint!.replaceAll('https:', 'wss:').replaceAll('http:', 'wss:')}/socket/websocket";
-        break;
-      default:
-        websocketEndpoint =
-            "${endpoint!.replaceAll('https:', 'wss:').replaceAll('http:', 'ws:')}/socket/websocket";
-        break;
-    }
-
-    final oracleRepository = ArchethicOracle(
-      phoenixHttpEndpoint: '$endpoint/socket/websocket',
-      websocketEndpoint: websocketEndpoint,
-    );
-
-    await oracleRepository.subscribeToOracleUpdates(
-      onUpdate: onUpdate,
-    );
-
-    return oracleRepository;
-  }
-
-  /// Close the subscription.
-  void closeOracleUpdatesSubscription(ArchethicOracle archethicOracle) {
-    archethicOracle.close();
-  }
+  Future<Stream<OracleUcoPrice>> subscribe() => _archethicOracle.subscribe();
 }
--- test/oracle_test.dart
+++ test/oracle_test.dart
@@ -1,7 +1,4 @@
-library test.oracle_test;
-
 // Project imports:
-import 'dart:async';
 import 'dart:developer';
 
 import 'package:archethic_lib_dart/archethic_lib_dart.dart';
@@ -17,11 +14,12 @@ void main() {
     tags: <String>[TestTags.noCI],
     () {
       test('getLastOracleUcoPrice', () async {
-        final oracleUcoPrice =
-            await OracleService('https://mainnet.archethic.net')
-                .getOracleData();
-        final oracleUcoPrice2 =
-            await OracleService('https://mainnet.archethic.net').getOracleData(
+        final oracleUcoPrice = await OracleService(
+          'https://mainnet.archethic.net',
+        ).getOracleData();
+        final oracleUcoPrice2 = await OracleService(
+          'https://mainnet.archethic.net',
+        ).getOracleData(
           timestamp: DateTime.now().millisecondsSinceEpoch ~/
               Duration.millisecondsPerSecond,
         );
@@ -32,72 +30,15 @@ void main() {
       test(
         'subscribeToOracleUpdates',
         () async {
-          final completer = Completer<void>();
+          final oracleStream = await OracleService(
+            'https://testnet.archethic.net',
+          ).subscribe();
 
-          await OracleService('https://testnet.archethic.net')
-              .subscribeToOracleUpdates((data) {
-            if (data == null) {
-              log('Oracle value null');
-            } else {
-              log('Oracle value: ${data.timestamp} - ${data.uco!.usd} USD');
-            }
-
-            if (!completer.isCompleted) {
-              completer.complete();
-            }
-          });
-
-          await Future.delayed(const Duration(minutes: 1), () {
-            if (!completer.isCompleted) {
-              log('Timeout reached');
-              completer.complete();
-            }
-          });
-        },
-        timeout: const Timeout(
-          Duration(minutes: 2),
-        ),
-      );
-
-      test(
-        'unsubscribeToOracleUpdates',
-        () async {
-          log('${DateTime.now().toIso8601String()} Start');
-
-          var updatesAfterUnsubscribe = false;
-
-          final oracleService =
-              await OracleService('https://mainnet.archethic.net')
-                  .subscribeToOracleUpdates((data) {
-            if (data == null) {
-              log('${DateTime.now().toIso8601String()} Oracle value null');
-            } else {
-              log('${DateTime.now().toIso8601String()} Oracle value: ${data.timestamp} - ${data.uco!.usd} USD');
-            }
-            updatesAfterUnsubscribe = true;
-          });
-
-          await Future.delayed(const Duration(minutes: 2), () {
-            log('${DateTime.now().toIso8601String()} Unsubscribe Oracle');
-            oracleService.close();
-            updatesAfterUnsubscribe = false;
-          });
-
-          await Future.delayed(const Duration(minutes: 2), () {
-            log('${DateTime.now().toIso8601String()} Checking for updates after unsubscribe');
-          });
-
-          expect(
-            updatesAfterUnsubscribe,
-            isFalse,
-            reason: 'Updates received after unsubscription',
+          final data = await oracleStream.first.timeout(
+            const Duration(minutes: 1),
           );
-
-          log('${DateTime.now().toIso8601String()} End');
+          log('Oracle value: ${data.timestamp} - ${data.uco!.usd} USD');
         },
-        timeout: const Timeout(
-          Duration(minutes: 10),
-        ),
       );
     },
   );
